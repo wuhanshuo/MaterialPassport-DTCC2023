cdbe9604e4a63cd6ee3667f03ba255700def52df
WU Han-Shuo <63944310+wuhanshuo@users.noreply.github.com>
2023-06-21 21:41:33 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 
 def findRecord(file,db,ID):
-    data =  {'db':'000','ID': '404', 'projectName': 'Not Found'}   # not found page defination
+    data =  {'db':'0','ID': '404', 'projectName': 'Not Found'}   # not found page defination
     with codecs.open(file) as f:
         for row in csv.DictReader(f, skipinitialspace=True):
             if row['ID'] == ID:
@@ -25,15 +25,15 @@ def index():
     get_id = request.args.get('id')
     db = request.args.get('db')
     if not (get_id and db):
-        data = {'db':'000','ID': '', 'projectName': 'Database Group', 'magic':'Congratulations, Sherlock! You have stumbled upon a mysterious hidden page.'}
+        data = {'db':'0','ID': '', 'projectName': 'Database Group', 'magic':'Congratulations, Sherlock! You have stumbled upon a mysterious hidden page.'}
     elif not (get_id.isdecimal() and get_id.isdecimal()):
-        data = {'db':'000','ID': '404', 'projectName': 'Not Found'}
+        data = {'db':'0','ID': '404', 'projectName': 'Not Found'}
     else:
         try:
             file = '/home/dtcc2023/mysite/static/database/{0}/{0}.csv'.format(db)
             data = findRecord(file,db,get_id)
         except:  # if the database path does not exist:
-            data = {'db':'000','ID': '404', 'projectName': 'Not Found'}
+            data = {'db':'0','ID': '404', 'projectName': 'Not Found'}
     return render_template("index.html", data=data)
 
 @app.route('/database')
