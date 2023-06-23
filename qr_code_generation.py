24ba3dd4cc1227e562aa70779cd1f1f720a516a9
Yannick Reich <yannick-reich@gmx.net>
2023-06-23 13:50:29 +0200

    Create qr_code_generation.py

---
+++ qr_code_generation.py
@@ -0,0 +1,25 @@
+import qrcode
+import qrcode.image.svg
+from qrcode.image.pure import PyPNGImage
+
+def generate_qr_code(project_id, item_id):
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=50,
+        border=4,
+        image_factory=PyPNGImage
+    )
+    project_id = str(project_id)
+    item_id = str(item_id)
+    qr.add_data("https://dtcc2023.pythonanywhere.com/?db=" + project_id + "&id=" + item_id)
+
+    print('https://dtcc2023.pythonanywhere.com/?db=' + project_id + '&id=' + item_id)
+    qr.make(fit=True)
+
+    img = qr.make_image().to_string(encoding='unicode')
+    img = img.replace("fill=\"#000000\"", "fill=\"#0000ff\"")
+    f = open("qrcodes/test.svg", "w")
+    f.write(img)
+    f.close()
+    print(type(img))
